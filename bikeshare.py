9c3a050ea01539cc8d387cfe795056a5226372b5
nguyen ngoc hieu <34811820+monkeeyhill@users.noreply.github.com>
2024-08-13 08:26:50 +0700

    refactoring:  remove all TODO
    refactoring:  remove all TODO

--- bikeshare.py
+++ bikeshare.py
@@ -10,7 +10,7 @@ def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
 
-#TODO: 
+
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
@@ -23,7 +23,6 @@ def get_filters():
     while city not in cities:
         city = input('Input city (chicago, new york city, washington): ').lower()
 
-#TODO: 
     # get user input for month (all, january, february, ... , june)
     months = ['all','january', 'february', 'march', 'april', 'may', 'june']
     month = input('Input month: ').lower()
